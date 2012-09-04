b64bf82ef73fa3bb7756a481c31efb49ca141c27
daniel <dmj@tid.es>
2012-09-04 12:45:23 +0200

    Recaptch for user registration

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,40 @@
-var model = require("../model");
+var model = require("../model"),
+    Recaptcha = require('recaptcha').Recaptcha,
+    PUBLIC_KEY = process.env.RECAPTCHA_PUBLIC_KEY,
+    PRIVATE_KEY = process.env.RECAPTCHA_PRIVATE_KEY;
 
-function showRegister(req, res) {
-    res.render('register', { title:'Eco.Sys: User registration page'});
+function showRegister(errorMessage) {
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
+
+    var renderData = {
+        title:'Eco.Sys: User registration page',
+        captcha: recaptcha.toHTML()
+    };
+
+    if (errorMessage) {
+        renderData.errorMessage = errorMessage;
+    }
+
+    return function(req, res) {
+        res.render('register', renderData);
+    }
 }
 
-function addUser(req, res) {
+function checkChallenge(req, callback) {
+    var data = {
+        remoteip:  req.connection.remoteAddress,
+        challenge: req.body.recaptcha_challenge_field,
+        response:  req.body.recaptcha_response_field
+    };
+
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
+
+    recaptcha.verify(function(success, error_code) {
+        callback((success)?null:error_code)
+    });
+}
+
+function createUser(req) {
     var User = model.mongoose.model('User');
     var userToRegister = new User();
 
@@ -19,12 +49,20 @@ function addUser(req, res) {
         if (!err)
             console.log('Success!');
         else
-            console.log('Ooooh' + err);
+            console.log('Ooooh ' + err);
     });
-
-    res.redirect('/login');
 }
 
-exports.registerForm = showRegister;
+function addUser(req, res) {
+    checkChallenge(req, function(err) {
+        if (err) {
+            showRegister(err)(req,res);
+        } else {
+            res.redirect('/login');
+        }
+    });
+}
+
+exports.registerForm = showRegister(null);
 exports.register = addUser;
 
